e3ca60012c408dd9020a4c03d4338a0434c20159
Luna R <snowluna@protonmail.com>
2022-05-01 14:16:13 +0000

    move markdown class to it's own file

---
+++ utils/markdown.js
@@ -0,0 +1,126 @@
+// A Markdown class because classes are fun imo
+class Markdown {
+   constructor() {
+      this.content = [];
+   }
+
+   // Add a header and text
+   addContent(headingText, bodyText, level = 2, inTable = false) {
+      let c = {
+         name: headingText,
+         text: bodyText,
+         level: level,
+         mdString: "",
+         table: inTable
+      }      
+      this.content.push(c);
+   }
+
+   // Add only a header
+   addHeader(h, level = 2, inTable = false) {
+      let header = {
+         name: h,
+         level: level,
+         table: inTable
+      }
+      this.content.push(header);
+   }
+
+   // Add only text
+   addText(t) {
+      let text = {
+         text: t
+      }
+      this.content.push(text);
+   }
+
+   // headingText: in case you want to use a unique toc name like "Contents"
+   addTable(headingText = "Table of Contents", level = 2) {
+      let t = {
+         name: headingText,
+         text: "",
+         level: level,
+         mdString: "",
+         isTable: true
+      }
+      this.content.push(t);
+   }
+
+   levelString(mdContentObj) {
+      const level = ["", "# ", "## ", "### ", "#### ", "##### ", "###### "];
+      let L = level[mdContentObj.level];
+      return L;
+   }
+
+   generate(m = this.content) {
+      var tocIndex = null;
+      const endl = "\n";
+
+      //
+      // Find table of contents
+      //
+      for (let i = 0; i < m.length; i++) {
+         if (m[i].isTable === true) {
+            tocIndex = i;
+            break;
+         }
+      }
+
+      // Don't need to continue if we don't have a table of contents
+      if (tocIndex === null) {
+         console.log("Did not find table of contents. Continuing...");
+      }
+      else {
+         // ...Create TOC string if we did find it, starting with the header
+         m.content[tocIndex].mdString = 
+            levelString(m.content[tocIndex])
+               + m.content[tocIndex].name + endl;
+         
+         // Generate table list string
+         var tableListStr = "";
+         for (let i = 0; i < m.length; i++) {
+            if (m[i].table === true) {
+               let name = m[i].name;
+               tableListStr += (`- [${name}](#${name.toLowerCase()})${endl}`);
+            }
+         }
+         // Add the table list to the markdown string
+         m.content[tocIndex].mdString += tableListStr;
+      }
+
+      //
+      // Generate Markdown for each object individually
+      //
+      for (let i = 0; i < m.length; i++) {
+         // Skip table of contents, it's special
+         if (i === tocIndex)
+            continue;
+
+         // String to hold all our Markdown
+         var markdown = "";
+         // Header
+         if (m[i].name) {
+            markdown += this.levelString(m[i]);
+            markdown += m[i].name + endl;
+         }
+         // Text
+         if (m[i].text) {
+            markdown += m[i].text + endl + endl;
+         }
+
+         m[i].mdString = markdown;
+      }
+   }
+
+   printMarkdown(m = this.content) {
+      var markdownString = "";
+      for (let i = 0; i < m.length; i++) {
+         if (m[i].mdString) {
+            markdownString += m[i].mdString;
+         }
+      }
+      console.log(markdownString);
+   }
+}
+
+module.exports = { Markdown };
